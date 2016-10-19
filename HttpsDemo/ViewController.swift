5eb308070496d5b74aaaba609fee11e2372f9bf4
SuddenKey <wanhao@wanhaodeimac.local>
2016-10-19 09:16:52 +0800

    Initial Commit

---
+++ HttpsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HttpsDemo
+//
+//  Created by 万浩 on 16/10/19.
+//  Copyright © 2016年 万浩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
